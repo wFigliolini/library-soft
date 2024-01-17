4994b70ac48ad3569a970104a0663917e6385d8d
Bill Figliolini <BillFigliolini@gmail.com>
2024-01-17 13:40:54 -0500

    Wrote tests for List

--- test.c
+++ test.c
@@ -19,18 +19,55 @@ int runTests(){
 }
 
 int runListTests(){
-    {
+    {   //Test for basic creation and destruction
         int TestVal1 = 1;
         ListPtr TestList1 = CreateIntList(TestVal1);
         assert(getIntVal(TestList1) == TestVal1);
         ListDestroy(&TestList1);
         assert(TestList1 == NULL);
     }
-    {
+    {   //Test for insertion and non-basic Destroy cases
         int testVal1 = 1; int testVal2 = 2;
         ListPtr TestList1 = CreateIntList(testVal1);
         ListInsertBack(TestList1, &testVal2);
-        assert()
+        assert(getIntVal(TestList1) == testVal1);
+        assert(getIntVal(TestList1->next) == testVal2);
+        assert(TestList1->next->next == NULL);
+        ListPtr SecondNode = TestList1->next;
+        ListDestroy(&TestList1);
+        assert(SecondNode == NULL);
+    }
+
+    {   //Test for deletion, Case 1: Head of List
+        int testVal1 = 1; int testVal2 = 2; int testVal3 = 3;
+        ListPtr TestList1 = CreateIntList(testVal1);
+        ListInsertBack(TestList1, &testVal2);
+        ListInsertBack(TestList1, &testVal3);
+        ListDelete(&TestList1, &testVal1);
+        assert(getIntVal(TestList1) == testVal2);
+        assert(getIntVal(TestList1->next) == testVal3);
+        ListDestroy(&TestList1);
+    }
+    {   //Test for deletion, Case 2: Tail of List
+        int testVal1 = 1; int testVal2 = 2; int testVal3 = 3;
+        ListPtr TestList1 = CreateIntList(testVal1);
+        ListInsertBack(TestList1, &testVal2);
+        ListInsertBack(TestList1, &testVal3);
+        ListDelete(&TestList1, &testVal3);
+        assert(getIntVal(TestList1) == testVal1);
+        assert(getIntVal(TestList1->next) == testVal2);
+        assert(TestList1->next->next == NULL);
+        ListDestroy(&TestList1);
+    }
+    {   //Test for deletion, Case 3: Center of List
+        int testVal1 = 1; int testVal2 = 2; int testVal3 = 3;
+        ListPtr TestList1 = CreateIntList(testVal1);
+        ListInsertBack(TestList1, &testVal2);
+        ListInsertBack(TestList1, &testVal3);
+        ListDelete(&TestList1, &testVal2);
+        assert(getIntVal(TestList1) == testVal1);
+        assert(getIntVal(TestList1->next) == testVal3);
+        ListDestroy(&TestList1);
     }
 
     return 0;
