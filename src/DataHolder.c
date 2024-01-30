71c2474b03a028cb7fe09eb0cf3247421742c9b7
Bill Figliolini <BillFigliolini@gmail.com>
2024-01-30 11:34:31 -0500

    Completed DataHolder Interface

--- src/DataHolder.c
+++ src/DataHolder.c
@@ -3,7 +3,7 @@
 
 struct datastruct{
     void* data;
-    //Comparison functions over data, returns 0, 1, -1. 1 for greater, 0 for equality, -1 for less 
+    //Comparison functions over data, returns 0, 1, -1. 1 for greater, 0 for equality, -1 for less. -2 is used for type mismatch
     int (*comparator)(void*,void*);
     void (*destroyer)(void*);
 };
@@ -21,4 +21,21 @@ void DestroyData(DataPtr* data){
     DataPtr curr = *data;
     curr->destroyer(curr->data);
     free(*data);
+}
+
+int CompareData(DataPtr left, DataPtr right){
+    if(left->comparator != right->comparator) return -2;
+    
+    int result = left->comparator(left->data, right->data); 
+
+    if( result > 0){
+        return 1;
+    }
+    if( result < 0){
+        return -1;
+    }
+    return 0;
+}
+void* GetData(DataPtr data){
+    return data->data;
 }
--- src/DataHolder.h
+++ src/DataHolder.h
@@ -7,5 +7,6 @@ typedef DataHolder* DataPtr;
 
 DataPtr CreateData(void* data, int (*comparator)(void*,void*), void (*destroyer)(void*));
 void    DestroyData(DataPtr* data);
-
+int     CompareData(DataPtr left, DataPtr right);
+void*   GetData(DataPtr data);
 #endif
